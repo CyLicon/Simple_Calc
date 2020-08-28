e3e98bc25855add5a6739df49c5f1a33ccaf7366
Kush Patel <68998452+Kush-bot@users.noreply.github.com>
2020-08-28 17:00:56 +0530

    Added feature: loop until you choose to exit

--- Simple_Calc.cpp
+++ Simple_Calc.cpp
@@ -2,44 +2,60 @@
 //
 
 #include <iostream>
+#include <stdlib.h>
 
 int main()
 {
+	std::cout << "Welcome to the C++ Calculator!\n";
+	int imagine = 2;
+	double v1 = 1;
+	double v2;
+	int op;
 	
-		double v1;
-		double v2;
-		double op;
-
-		std::cout << "Welcome to the C++ Calculator\n";
-		std::cout << "Enter your first digit\n";
+	while (v1 != 0) {
+		
+		std::cout << "Enter your first number or press 0 to exit.\n";
 		std::cin >> v1;
 
-		std::cout << "Enter your second digit\n";
-		std::cin >> v2;
+		if (v1 != 0) {
 
-		std::cout << "Choose your operation\n";
-		std::cout << " 1)Add  2)Subtract  3)Multiply  4)Divide \n";
-		std::cin >> op;
 
-		if (op == 1) {
-			std::cout << "the answer is: " << v1 + v2 << "\n";
-		}
-		else if (op == 2) {
-			std::cout << "the answer is: " << v1 - v2 << "\n";
-		}
-		else if (op == 3) {
-			std::cout << "the answer is: " << v1 * v2 << "\n";
-		}
-		else if (op == 4) {
-			std::cout << "the answer is: " << v1 / v2 << "\n";
-		}
-		else {
-			std::cout << "Error: Invalid operation. Please choose from the four options given above.\n";
-		}
-		std::cout << "Thanks for using calc!\n" << std::endl;
+				std::cout << "Enter your next number: \n";
+				std::cin >> v2;
 
-		return main();
+				std::cout << "Choose your operation\n";
+				std::cout << " 1)Add  2)Subtract  3)Multiply  4)Divide \n";
+				std::cin >> op;
+
+				switch(op) {
+
+				case 1:
+					std::cout << "Your answer is: " << v1 + v2 << "\n";
+					break;
+				case 2:
+					std::cout << "Your answer is: " << v1 - v2 << "\n";
+					break;
+				case 3:
+					std::cout << "Your answer is: " << v1 * v2 << "\n";
+					break;
+				case 4:
+					std::cout << "Your answer is: " << v1 / v2 << "\n";
+					break;
+				default:
+					std::cout << "Error: Invalid operation or character. Please type correct numericals and choose from the four options given above.\n";
+					break;
+				}
+
+					}
+		else 
+		{
+			std::cout << "Thanks for using calc!\n" << std::endl;
+			void exit();
+		}
+
 	}
+}
+	
 
 	// Run program: Ctrl + F5 or Debug > Start Without Debugging menu
 	// Debug program: F5 or Debug > Start Debugging menu
